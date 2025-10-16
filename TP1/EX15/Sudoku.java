d4f15d83412a0fdbc6ef10cfdc4d19bacd6d0854
Yann Miquel--Erdmann <yann.miquelerdmann@gmail.com>
2025-10-16 09:04:32 +0200

    Déplacement vers TP1

---
+++ TP1/EX15/Sudoku.java
@@ -0,0 +1,22 @@
+package EX15;
+
+public class Sudoku {
+    static void printGrid(int[] grid){
+        for (ig)
+    }
+    static backtrackingSolver()
+    public static void main(String[] args) {
+        
+        int grid[] = {
+                0, 1, 0, 6, 0, 7, 0, 0, 4,
+                0, 4, 2, 0, 0, 0, 0, 0, 0,
+                8, 7, 0, 3, 0, 0, 6, 0, 0,
+                0, 8, 0, 0, 7, 0, 0, 2, 0,
+                0, 0, 0, 8, 9, 3, 0, 0, 0,
+                0, 3, 0, 0, 6, 0, 0, 1, 0,
+                0, 0, 8, 0, 0, 6, 0, 4, 5,
+                0, 0, 0, 0, 0, 0, 1, 7, 0,
+                4, 0, 0, 9, 0, 8, 0, 6, 0
+        };
+    }
+}
